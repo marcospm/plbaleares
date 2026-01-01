ef2bc47851cbf591eb0c02fa1a5bbb780399947c
Marcos <mpacheco1988@gmail.com>
2026-01-01 21:08:11 +0100

    doctrine

--- public/index.php
+++ public/index.php
@@ -1,8 +1,30 @@
 <?php
 
 use App\Kernel;
+use Platformsh\ConfigReader\Config;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
+/**
+ * Platform.sh – generate DATABASE_URL from relationships
+ */
+if (getenv('PLATFORM_RELATIONSHIPS') && !getenv('DATABASE_URL')) {
+    $config = new Config();
+    $creds = $config->credentials('database');
+
+    $dsn = sprintf(
+        'mysql://%s:%s@%s:%s/%s?charset=utf8mb4',
+        rawurlencode($creds['username']),
+        rawurlencode($creds['password']),
+        $creds['host'],
+        $creds['port'],
+        ltrim($creds['path'], '/')
+    );
+
+    putenv("DATABASE_URL=$dsn");
+    $_ENV['DATABASE_URL'] = $dsn;
+    $_SERVER['DATABASE_URL'] = $dsn;
+}
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
